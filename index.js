859190bf6d7eba41428a077c0a09a2a5d65120f5
Cristhian Rodríguez <crisrodam1996@gmail.com>
2026-05-27 22:52:09 -0500

    Correcion de lista de puntos de venta

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ server.listen(envsConfig.PORT, () => {
   sq.sync({
     logging: false,
     force: false, // Cambiado a false para no borrar tus tickets ganadores
-    alter: false,
+    alter: true,
   })
     .then(() => {
       console.log('Base de datos sincronizada con éxito')
